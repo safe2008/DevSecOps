24ca8d99cef05972c16ff18c9974f9e4226b72de
safe2008 <boriphuth.sae@truedigital.com>
2021-01-04 20:19:13 +0700

    Update

--- dependency-check.sh
+++ dependency-check.sh
@@ -32,7 +32,7 @@ PROJECT_ID=`archerysec-cli -s $ARCHERYSEC_HOST -u $ARCHERYSEC_USER -p $ARCHERYSE
 echo $PROJECT_ID
 
 SCAN_ID=`archerysec-cli -s $ARCHERYSEC_HOST -u $ARCHERYSEC_USER -p $ARCHERYSEC_PASS --upload --file_type=XML \
---file=odc-reports/dependency-check-report.xml --TARGET=$GITHUB_SHA --scanner=dependencycheck \
+--file=reports/dependency-check-report.xml --TARGET=$GITHUB_SHA --scanner=dependencycheck \
 --project_id=$PROJECT_ID | tail -n1 | jq '.scan_id' | sed -e 's/^"//' -e 's/"$//'`
 sleep 20
 
