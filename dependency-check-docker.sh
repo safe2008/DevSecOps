8ef976be415dce1965cace8c9e3febb6472cc642
safe2008 <boriphuth.sae@truedigital.com>
2021-01-04 21:04:56 +0700

    Update

--- dependency-check-docker.sh
+++ dependency-check-docker.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+DC_VERSION="latest"
 DC_DIRECTORY=$HOME/OWASP-Dependency-Check
 DC_PROJECT="dependency-check scan: $(pwd)"
 DATA_DIRECTORY="$DC_DIRECTORY/data"
@@ -14,25 +15,22 @@ if [ ! -d "$CACHE_DIRECTORY" ]; then
     mkdir -p "$CACHE_DIRECTORY"
 fi
 
-echo $DC_VERSION
-echo $DATA_DIRECTORY
-echo $DC_PROJECT
-echo $USER
-
 # Make sure we are using the latest version
 docker pull owasp/dependency-check:$DC_VERSION
 
 docker run --rm \
--e user=$USER \
--u $(id -u ${USER}):$(id -g ${USER}) \
---volume $(pwd):/src:z \
---volume $DATA_DIRECTORY:/usr/share/dependency-check/data:z \
---volume $(pwd)/odc-reports:/report:z \
-owasp/dependency-check:$DC_VERSION \
---scan /src \
---format "ALL" \
---project $DC_PROJECT \
---out /report
+    -e user=$USER \
+    -u $(id -u ${USER}):$(id -g ${USER}) \
+    --volume $(pwd):/src:z \
+    --volume "$DATA_DIRECTORY":/usr/share/dependency-check/data:z \
+    --volume $(pwd)/odc-reports:/report:z \
+    owasp/dependency-check:$DC_VERSION \
+    --scan /src \
+    --format "ALL" \
+    --project "$DC_PROJECT" \
+    --out /report
+    # Use suppression like this: (where /src == $pwd)
+    # --suppression "/src/security/dependency-check-suppression.xml"
 
 cat odc-reports/dependency-check-report.xml
 # sudo apt-get install jq
